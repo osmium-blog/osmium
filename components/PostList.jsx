0eff5f030e8ebb73a4b5257694cf69674bc181d2
SilentDepth <silentdepth@outlook.com>
2023-04-11 20:44:23 +0800

    🚑 reading prop of undefined

--- components/PostList.jsx
+++ components/PostList.jsx
@@ -3,7 +3,7 @@ import PostListItem from '@/components/PostListItem'
 export default function PostList ({ posts }) {
   return (
     <ul className="divide-y divide-gray-300 dark:divide-gray-700">
-      {posts.map(post => (
+      {posts?.map(post => (
         <li key={post.id} className="py-5">
           <PostListItem post={post}/>
         </li>
