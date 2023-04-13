674f1d4d37d9d578ab2081409274d8eddee084f9
SilentDepth <silentdepth@outlook.com>
2023-04-13 10:59:39 +0800

    🎨 minor reformat

--- components/Header.tsx
+++ components/Header.tsx
@@ -98,14 +98,16 @@ function SiteTitle ({ pageTitle }: SiteTitleProps) {
 }
 
 function NavBar () {
-  const BLOG = useConfig()
+  const { path, showAbout } = useConfig()
   const locale = useLocale()
+
   const links = [
-    { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
-    { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
+    { id: 0, name: locale.NAV.INDEX, to: path || '/', show: true },
+    { id: 1, name: locale.NAV.ABOUT, to: '/about', show: showAbout },
     { id: 2, name: locale.NAV.RSS, to: '/-/feed', show: true },
     { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true },
   ]
+
   return (
     <div className="flex-shrink-0">
       <ul className="flex flex-row">
