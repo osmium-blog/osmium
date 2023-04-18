2a48a0ea3d22d8f9c9ad80a0c10657bb087c003d
SilentDepth <silentdepth@outlook.com>
2023-04-18 14:40:49 +0800

    💄 tweak post layout for post summary

--- layouts/docs/post.tsx
+++ layouts/docs/post.tsx
@@ -1,24 +1,19 @@
 import type { ExtendedRecordMap } from 'notion-types'
-import cn from 'classnames'
 
 import css from './styles.module.scss'
-import type { PageProps } from '@/lib/server/notion-api/page'
-import { useConfig } from '@/contexts/config'
+import type { PageMeta } from '@/lib/server/notion-api/page'
 import useTheme from '@/contexts/theme'
 import NotionRenderer from '@/components/NotionRenderer'
 import TableOfContents from '@/components/TableOfContents'
-import UserAvatar from '@/components/UserAvatar'
 import FormattedDate from '@/components/FormattedDate'
-import TagItem from '@/components/TagItem'
 import Comments from '@/components/comments'
 
 type Props = {
-  post: PageProps
+  post: PageMeta
   recordMap: ExtendedRecordMap
 }
 
 export default function SlugLayout ({ post, recordMap }: Props) {
-  const config = useConfig()
   const { dark } = useTheme()
 
   const { title, type, date, tags } = post
@@ -27,27 +22,19 @@ export default function SlugLayout ({ post, recordMap }: Props) {
     <div className={css.post_layout}>
       <article>
         <h1 className={css.post_title}>{title}</h1>
-        {type !== 'Page' && (
-          <div className={css.post_info}>
-            <div className="mr-2 mb-4 md:ml-0">
-              Last updated:&nbsp;
-              <FormattedDate date={date}/>
-            </div>
+        {post.summary && <p className={css.post_summary}>{post.summary}</p>}
+        <div className={css.post_info}>
+          <div className={css.post_update_time}>
+            <span>Last update:&nbsp;</span>
+            <FormattedDate date={date}/>
           </div>
-        )}
+        </div>
         <div className={css.post_content}>
           <NotionRenderer recordMap={recordMap} fullPage={false} darkMode={dark}/>
         </div>
       </article>
       <aside>
-        {/* `65px` is the height of expanded nav */}
-        {/* Plus 16px to keep margin */}
-        {/* TODO: Remove the magic number */}
-        <TableOfContents
-          recordMap={recordMap}
-          className="lg:inline-block sticky lg:mr-4"
-          style={{ top: 65 + 16 + 'px' }}
-        />
+        <TableOfContents recordMap={recordMap} className={css.post_toc}/>
       </aside>
       <Comments post={post} className={css.post_comments}/>
     </div>
