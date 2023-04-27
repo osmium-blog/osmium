cc0b19e27d02cadaadb846e3052980938afbe9bb
SilentDepth <silentdepth@outlook.com>
2023-04-27 19:59:43 +0800

    🐛(docs-layout) page meta is missing

--- layouts/docs/root.tsx
+++ layouts/docs/root.tsx
@@ -1,15 +1,19 @@
 import type { ReactNode } from 'react'
 import { useRouter } from 'next/router'
 
+import { useData } from '@/contexts/data'
 import css from './styles.module.scss'
 import Header from './header'
+import LayoutHead from '@/layouts/blog/head'
 import LayoutNav from './nav'
 import LayoutFooter from './footer'
 
 export default function DocsLayout ({ children }: { children: ReactNode }) {
   const router = useRouter()
+  const { current: post } = useData()
 
-  return (
+  return <>
+    <LayoutHead post={post}/>
     <div data-layout-root={true} data-page={router.pathname} className={css.layout}>
       <Header className={css.layout_header}/>
       <main className={css.layout_main}>
@@ -22,5 +26,5 @@ export default function DocsLayout ({ children }: { children: ReactNode }) {
         </div>
       </main>
     </div>
-  )
+  </>
 }
