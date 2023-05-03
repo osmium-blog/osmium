8666d1573734c753985e0c3318c24545d3e02a99
SilentDepth <silentdepth@outlook.com>
2023-05-03 21:42:53 +0800

    🐛(docs-layout) docs nav not working in Firefox

--- layouts/docs/root.tsx
+++ layouts/docs/root.tsx
@@ -1,3 +1,4 @@
+import { createContext, useContext, useState } from 'react'
 import type { ReactNode } from 'react'
 import { useRouter } from 'next/router'
 
@@ -8,23 +9,41 @@ import LayoutHead from '@/layouts/blog/head'
 import LayoutNav from './nav'
 import LayoutFooter from './footer'
 
+const LayoutStateContext = createContext(undefined as any)
+
+export function useLayoutState () {
+  return useContext(LayoutStateContext)
+}
+
 export default function DocsLayout ({ children }: { children: ReactNode }) {
   const router = useRouter()
   const { current: post } = useData()
 
-  return <>
-    <LayoutHead post={post}/>
-    <div data-layout-root={true} data-page={router.pathname} className={css.layout}>
-      <Header className={css.layout_header}/>
-      <main className={css.layout_main}>
-        <nav className={css.layout_nav}>
-          <LayoutNav/>
-        </nav>
-        <div className={css.layout_page}>
-          {children}
-          <LayoutFooter/>
-        </div>
-      </main>
-    </div>
-  </>
+  const [hasNav, setHasNav] = useState(false)
+  const layoutState = {
+    hasNav: setHasNav,
+  }
+
+  return (
+    <LayoutStateContext.Provider value={layoutState}>
+      <LayoutHead post={post}/>
+      <div
+        data-layout-root={true}
+        data-page={router.pathname}
+        data-no-nav={hasNav ? null : 'true'}
+        className={css.layout}
+      >
+        <Header className={css.layout_header}/>
+        <main className={css.layout_main}>
+          <nav className={css.layout_nav}>
+            <LayoutNav/>
+          </nav>
+          <div className={css.layout_page}>
+            {children}
+            <LayoutFooter/>
+          </div>
+        </main>
+      </div>
+    </LayoutStateContext.Provider>
+  )
 }
