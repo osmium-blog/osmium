f5cb5d21c96611e71bd29e79d2ed3db8d3980a82
Aron Yang <impixelhu@gmail.com>
2025-05-06 16:53:33 +0800

    🐛 make Cusdis follow resolved light/dark scheme

--- components/comments/cusdis.jsx
+++ components/comments/cusdis.jsx
@@ -1,18 +1,21 @@
 import { ReactCusdis } from 'react-cusdis'
 import { useRouter } from 'next/router'
 import { useConfig } from '@/contexts/config'
+import { useTheme } from "@/contexts/theme";
 
 export default function Cusdis ({ config, post }) {
   const router = useRouter()
-  const { link, lang, appearance } = useConfig()
+  const { link, lang } = useConfig()
+  const { scheme } = useTheme()
 
   return (
     <ReactCusdis
+      key={`cusdis-${scheme}`}
       attrs={{
         pageId: post.id,
         pageTitle: post.title,
         pageUrl: link + router.asPath,
-        theme: appearance,
+        theme: scheme,
         ...config,
       }}
       lang={resolveLang(lang)}
